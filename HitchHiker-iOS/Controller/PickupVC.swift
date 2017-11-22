1e65fa8e5b9c69d58fbf64efd80c01c28d5e1f23
mikezander <mikezander87@yahoo.com>
2017-11-22 16:41:36 -0500

    allowed drivers to accept trips

--- HitchHiker-iOS/Controller/PickupVC.swift
+++ HitchHiker-iOS/Controller/PickupVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Firebase
 
 class PickupVC: UIViewController {
 
@@ -21,6 +22,8 @@ class PickupVC: UIViewController {
     
     var locationPlacemark: MKPlacemark!
     
+    var currentUserId = Auth.auth().currentUser?.uid
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pickupMapView.delegate = self
@@ -29,6 +32,16 @@ class PickupVC: UIViewController {
         
         dropPinForPlacemark(placemark: locationPlacemark)
         centerMapOnLocation(location: locationPlacemark.location!)
+        
+        DataService.instance.REF_TRIPS.child(passengerKey).observe(.value) { (tripSnapshot) in
+            if tripSnapshot.exists() {
+                if tripSnapshot.childSnapshot(forPath: "tripIsAccepted").value as? Bool == true {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            } else {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
     func initData(coordinate: CLLocationCoordinate2D, passengerKey: String) {
@@ -37,6 +50,8 @@ class PickupVC: UIViewController {
     }
 
     @IBAction func acceptTripBtnWasPressed(_ sender: Any) {
+       UpdateService.instance.acceptTrip(withPassengerKey: passengerKey, forDriverKey: currentUserId!)
+        presentingViewController?.shouldPresentLoadingView(true)
         
     }
     @IBAction func cancelBtnWasPressed(_ sender: Any) {
